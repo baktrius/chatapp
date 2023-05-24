9dadac93addff83322b8089460c282cd7504e4dd
Krzysztof Rogowski <krzysztor99@gmail.com>
2023-05-24 08:32:14 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Simple websocket chat app
+
+Based on this [article](https://dev.to/devland/build-a-real-time-chat-app-using-nodejs-and-websocket-441g). Unfortunately repo mentioned there seems to be gone.
+
+Code is slightly modified.
---
+++ index.mjs
@@ -0,0 +1,28 @@
+import express from 'express';
+import ws, { WebSocketServer } from 'ws';
+
+const PORT = process.argv[2] || 8080
+
+const app = express();
+//serve static folder
+app.use(express.static('public'))
+
+const server = app.listen(PORT, () => {
+    console.log(`http://localhost:${PORT}`)
+});
+
+const wss = new WebSocketServer({ server }) // (2)
+wss.on('connection', (client) => {
+    console.log('Client connected !')
+    client.on('message', (msg) => {    // (3)
+        console.log(`Message:${msg}`);
+        broadcast(toString(msg))
+    })
+})
+function broadcast(msg) {       // (4)
+    for (const client of wss.clients) {
+        if (client.readyState === ws.OPEN) {
+            client.send(msg)
+        }
+    }
+}
