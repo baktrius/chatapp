634d49d57f9498e1f9cfec91deb202c23eee7ad8
Krzysztof Rogowski <krzysztor99@gmail.com>
2023-05-30 21:48:43 +0200

    full solution implementation
    added images upload and users system

--- index.mjs
+++ index.mjs
@@ -1,25 +1,76 @@
 import express from 'express';
+import multer from 'multer';
+import session from 'express-session';
 import ws, { WebSocketServer } from 'ws';
+
+const upload = multer({ dest: 'uploads/' })
 
 const PORT = process.argv[2] || 8080
 
 const app = express();
-//serve static folder
-app.use(express.static('public'))
 
+// session library config
+app.use(session({
+    secret: 'keyboard cat',
+    cookie: { maxAge: 60000 },
+    resave: false, // don't save session if unmodified
+    saveUninitialized: false, // don't create session until something stored
+}))
+
+// paths which can be reached by not logged users
+const publicUrls = new Set(["/login.html", "/login"])
+// redirect not logged users to login.html
+app.use(function (req, res, next) {
+    if (!publicUrls.has(req.path) && req.session.login === undefined) {
+        res.redirect('/login.html');
+    } else {
+        next();
+    }
+});
+
+// enable post request data parsing
+app.use(express.urlencoded())
+
+// handle login
+app.post('/login', (req, res) => {
+    req.session.login = req.body.login
+    res.redirect("/")
+})
+// handle logout
+app.post('/logout', (req, res) => {
+    req.session.destroy()
+    res.redirect("login.html")
+})
+
+// serve static folder
+app.use(express.static('public'))
+// server images uploaded by users
+app.use('/uploads', express.static('uploads'))
+
+// handle image upload
+app.post('/img', upload.single('img'), function (req, res, next) {
+    res.json({ success: true })
+    broadcast(JSON.stringify({ img: req.file.path }))
+    next()
+})
+
+// start http server
 const server = app.listen(PORT, () => {
     console.log(`http://localhost:${PORT}`)
 });
 
-const wss = new WebSocketServer({ server }) // (2)
+// socket server configuration
+// ! ws don't handle authorization
+const wss = new WebSocketServer({ server })
 wss.on('connection', (client) => {
     console.log('Client connected !')
-    client.on('message', (msg) => {    // (3)
-        console.log(`Message:${msg}`);
-        broadcast(msg)
+    client.on('message', (msg, isBinary) => {
+        console.log(`Message:${msg}`, isBinary);
+        broadcast(JSON.stringify({ text: msg.toString() }))
     })
 })
-function broadcast(msg) {       // (4)
+
+function broadcast(msg) {
     for (const client of wss.clients) {
         if (client.readyState === ws.OPEN) {
             client.send(msg)
--- public/script.js
+++ public/script.js
@@ -1,5 +1,4 @@
 const ws = new WebSocket(`ws://${window.document.location.host}`);
-ws.binaryType = "blob";
 // Log socket opening and closing
 ws.addEventListener("open", event => {
     console.log("Websocket connection opened");
@@ -7,26 +6,57 @@ ws.addEventListener("open", event => {
 ws.addEventListener("close", event => {
     console.log("Websocket connection closed");
 });
-ws.onmessage = function (message) {
+
+function addMessage(val) {
     const msgDiv = document.createElement('div');
     msgDiv.classList.add('msgCtn');
-    if (message.data instanceof Blob) {
-        reader = new FileReader();
-        reader.onload = () => {
-            msgDiv.innerHTML = reader.result;
-            document.getElementById('messages').appendChild(msgDiv);
-        };
-        reader.readAsText(message.data);
-    } else {
-        console.log("Result2: " + message.data);
-        msgDiv.innerHTML = message.data;
-        document.getElementById('messages').appendChild(msgDiv);
+    msgDiv.innerHTML = val;
+    document.getElementById('messages').appendChild(msgDiv);
+}
+
+function addImg(path) {
+    const msgDiv = document.createElement('img');
+    msgDiv.classList.add('msgCtn');
+    msgDiv.src = path
+    document.getElementById('messages').appendChild(msgDiv);
+}
+
+ws.onmessage = function (message) {
+    try {
+        const data = JSON.parse(message.data)
+        console.log(data)
+        if (data.img !== undefined) addImg(data.img)
+        if (data.text != undefined) addMessage(data.text)
+    }
+    catch(err) {
+        console.error(err)
     }
 }
 const form = document.getElementById('msgForm');
 form.addEventListener('submit', (event) => {
     event.preventDefault();
-    const message = document.getElementById('inputBox').value;
+    const form = event.target;
+    const formData = new FormData(form);
+    const message = formData.get('message')
     ws.send(message);
-    document.getElementById('inputBox').value = ''
+    form.reset();
+})
+const imgForm = document.getElementById('imgForm');
+imgForm.addEventListener('submit', async (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const formData = new FormData(form);
+    try {
+        // send appropriate request
+        // long time can pass between calling fetch and getting response
+        const response = await fetch(form.action, { method: 'post', body: formData });
+        // parse response as json
+        const data = await response.json();
+        console.log(data);
+        if (data.success) form.reset()
+        // display added comment at current page (without reloading)
+    } catch (err) {
+        // best error handling
+        alert(`something went wrong during form upload ${err}`);
+    }
 })
